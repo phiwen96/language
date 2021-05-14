12aa7d9687247fecd55563e41eae8b225a358e16
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-05-14 11:57:24 +0200

    a

---
+++ include/language/language.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <experimental/coroutine>
+using namespace std::experimental;
+using namespace std;
+
+
+
+struct task
+{
+    struct promise_type
+    {
+        auto 
+    };
+    
+    promise_type& m_promise;
+    
+};
+
+
+
+
---
+++ tests/include/test.hpp
@@ -0,0 +1,17 @@
+#include <catch2/catch.hpp>
+
+//using namespace std;
+#include <experimental/coroutine>
+using namespace std::experimental;
+/**
+ bool = types <int, char, string> | identical
+ bool types <int, char, string> | distinct
+ */
+
+
+auto run () -> int 
+{
+
+}
+
+
